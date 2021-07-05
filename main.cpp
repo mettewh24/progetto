6690a3195517def5c0ad985f6faf6f5777d96e39
Matteo Bonazzi <matteo.bonazzi2@studio.unibo.it>
2021-07-05 17:22:34 +0200

    Added writing on file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 
@@ -10,12 +11,28 @@ void print(std::vector<World_state> const& state) {
                "+-----------+-----------+-----------+-----------+\n"
             << std::fixed;
   for (auto& state : state) {
-    std::cout <<std::setprecision(0)<< "| " << std::setw(9) << state.S << " | " << std::setw(9)
-              << state.I << " | " << std::setw(9) << state.R << " | "
-              << std::setw(9) << state.N << " |\n";
+    std::cout << std::setprecision(0) << "| " << std::setw(9) << state.S
+              << " | " << std::setw(9) << state.I << " | " << std::setw(9)
+              << state.R << " | " << std::setw(9) << state.N << " |\n";
   }
   std::cout << "+-----------+-----------+-----------+-----------+\n";
 }
+
+void print_on_file(std::vector<World_state> const& state) {
+  std::ofstream fout("pandemic.txt");
+  fout << "+-----------+-----------+-----------+-----------+\n"
+          "|     S     |     I     |     R     |     N     |\n"
+          "+-----------+-----------+-----------+-----------+\n"
+       << std::fixed;
+  for (auto& state : state) {
+    fout << std::setprecision(0) << "| " << std::setw(9) << state.S << " | "
+         << std::setw(9) << state.I << " | " << std::setw(9) << state.R << " | "
+         << std::setw(9) << state.N << " |\n";
+  }
+  fout << "+-----------+-----------+-----------+-----------+\n";
+
+  fout.close();
+};
 
 int main() {
   World_state initial_state;
@@ -30,5 +47,28 @@ int main() {
 
   Pandemic sir{initial_state, duration_in_days};
   auto a = sir.evolve();
-  print(a);
+  
+  
+  char choice;
+  std::cin >> choice;
+  choice=static_cast<int>(choice);
+  switch (choice) {
+    case 110:// corrisponde al char n(no)
+      print(a);
+      break;
+
+    case 97://corrisponde al char a (all)
+      print(a);
+      print_on_file(a);
+      break;
+
+    case 121://corrisponde al char y (yes)
+      print_on_file(a);
+      break;
+
+    default:
+      print(a);
+      print_on_file(a);
+      break;
+  }
 }
