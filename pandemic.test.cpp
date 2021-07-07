5a8820426816ba3f9f5881ad1a297affe41b6742
Max <massimo.dschmidt@gmail.com>
2021-07-07 23:08:33 +0200

    Added some tests

--- pandemic.test.cpp
+++ pandemic.test.cpp
@@ -35,6 +35,52 @@ TEST_CASE("Testing") {
     CHECK(a[1].beta == s1.beta);
   }
 
+    SUBCASE("testing evolve 2") {
+    World_state s00{145050,5000,0,0.5,0.6,150050};
+
+    World_state s01{142633,4417,3000,0.5,0.6,150050};
+
+    World_state s02{140534,3866,5650,0.5,0.6,150050};
+
+    World_state s03{138724,3357,7969,0.5,0.6,150050};
+
+    World_state s04{137172,2894,9984,0.5,0.6,150050};
+
+    World_state s05{135849,2481,11720,0.5,0.6,150050};
+    
+
+    Pandemic test00(s00, 1);
+    auto b = evolve(test00.get_state(),test00.get_duration());
+    b[1]=approx(b[1]);
+
+    CHECK(b[1] == s01);
+    
+    Pandemic test01(s00, 2);
+    auto c = evolve(test01.get_state(),test01.get_duration());
+    c[2]=approx(c[2]);
+
+    CHECK(c[2] == s02);
+
+    Pandemic test02(s00, 3);
+    auto d = evolve(test02.get_state(),test02.get_duration());
+    d[3]=approx(d[3]);
+    
+    CHECK(d[3] == s03);
+
+    Pandemic test03(s00, 4);
+    auto e = evolve(test03.get_state(),test03.get_duration());
+    e[4]=approx(e[4]);
+    
+    CHECK(e[4] == s04);
+
+    Pandemic test04(s00, 5);
+    auto f = evolve(test04.get_state(),test04.get_duration());
+    f[5]=approx(f[5]);
+    
+    CHECK(f[5] == s05);
+
+  }
+
   SUBCASE("testing exeptions") {
     World_state s0{};
     s0.S = 0;
