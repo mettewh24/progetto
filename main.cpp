30267b883455b2ec1ed82ac673d802bdd53ca08f
Matteo Bonazzi <matteo.bonazzi2@studio.unibo.it>
2021-07-09 17:20:16 +0200

    Fixed problems with the formatting of output data

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <fstream>
 #include <iomanip>
@@ -5,40 +6,104 @@
 
 #include "pandemic.prova1.hpp"
 //#include"pandemic.hpp"
-
+int number_of_digits(int const number) {
+  int i = 1;
+  int N = number;
+  for (; N / 10 >= 1; ++i) {
+    N = N / 10;
+  }
+  return i;
+}
+namespace print {
 void print(std::vector<World_state> const& state) {
-  std::cout << "+-----------+-----------+-----------+-----------+-----------+\n"
-               "|    Day    |     S     |     I     |     R     |     N     |\n"
-               "+-----------+-----------+-----------+-----------+-----------+\n"
-            << std::fixed;
+  int max_lenght =
+      std::max(number_of_digits(state[0].N), number_of_digits(state.size()));
+  if (max_lenght % 2 == 0) {
+    ++max_lenght;
+  }
+  if (max_lenght < 5) {
+    max_lenght = 5;
+  }
+  std::string bar = "+";
+  std::string legend = "|";
+  std::vector<std::string> variables = {"Day", "S", "I", "R", "N", "B", "G"};
+  for (int b = 0; b < 7; ++b) {
+    for (int a = 1; a <= max_lenght + 2; ++a) {
+      bar.append("-");
+    }
+    for (int c = 0;
+         c <= static_cast<int>((max_lenght - variables[b].length()) / 2); ++c) {
+      legend.append(" ");
+    }
+    legend.append(variables[b]);
+    for (int c = 0;
+         c <= static_cast<int>((max_lenght - variables[b].length()) / 2); ++c) {
+      legend.append(" ");
+    }
+    bar.append("+");
+    legend.append("|");
+  }
+  std::cout << bar << '\n' << legend << '\n' << bar << '\n';
   int size = state.size();
   for (int i = 0; i < size; ++i) {
-    std::cout << std::setprecision(0) << "| " << std::setw(9) << i << " | "
-              << std::setw(9) << state[i].S << " | " << std::setw(9)
-              << state[i].I << " | " << std::setw(9) << state[i].R << " | "
-              << std::setw(9) << state[i].S + state[i].I + state[i].R << " |\n";
+    std::cout << "| " << std::setw(max_lenght) << i
+              << " | " << std::setw(max_lenght) << state[i].S << " | "
+              << std::setw(max_lenght) << state[i].I << " | "
+              << std::setw(max_lenght) << state[i].R << " | "
+              << std::setw(max_lenght) << state[i].S + state[i].I + state[i].R
+              << " | " << std::setw(max_lenght) << state[i].beta << " | "
+              << std::setw(max_lenght) << state[i].gamma << " | " << '\n';
   }
-  std::cout
-      << "+-----------+-----------+-----------+-----------+-----------+\n";
+  std::cout << bar << "\n";
 }
+}  // namespace print
 
+namespace print_on_file {
 void print_on_file(std::vector<World_state> const& state) {
   std::ofstream ostrm("pandemic.txt");
-  ostrm << "+-----------+-----------+-----------+-----------+-----------+\n"
-           "|    Day    |     S     |     I     |     R     |     N     |\n"
-           "+-----------+-----------+-----------+-----------+-----------+\n"
-        << std::fixed;
+  int max_lenght =
+      std::max(number_of_digits(state[0].N), number_of_digits(state.size()));
+  if (max_lenght < 5) {
+    max_lenght = 5;
+  }
+  if (max_lenght % 2 == 0) {
+    ++max_lenght;
+  }
+  std::string bar = "+";
+  std::string legend = "|";
+  std::vector<std::string> variables = {"Day", "S", "I", "R", "N", "B", "G"};
+  for (int b = 0; b < 7; ++b) {
+    for (int a = 1; a <= max_lenght + 2; ++a) {
+      bar.append("-");
+    }
+    for (int c = 0;
+         c <= static_cast<int>((max_lenght - variables[b].length()) / 2); ++c) {
+      legend.append(" ");
+    }
+    legend.append(variables[b]);
+    for (int c = 0;
+         c <= static_cast<int>((max_lenght - variables[b].length()) / 2); ++c) {
+      legend.append(" ");
+    }
+    bar.append("+");
+    legend.append("|");
+  }
+  ostrm << bar << '\n' << legend << '\n' << bar << '\n';
   int size = state.size();
   for (int i = 0; i < size; ++i) {
-    ostrm << std::setprecision(0) << "| " << std::setw(9) << i << " | "
-          << std::setw(9) << state[i].S << " | " << std::setw(9) << state[i].I
-          << " | " << std::setw(9) << state[i].R << " | " << std::setw(9)
-          << state[i].N << " |\n";
+    ostrm << "| " << std::setw(max_lenght) << i << " | "
+          << std::setw(max_lenght) << state[i].S << " | "
+          << std::setw(max_lenght) << state[i].I << " | "
+          << std::setw(max_lenght) << state[i].R << " | "
+          << std::setw(max_lenght) << state[i].S + state[i].I + state[i].R
+          << " | " << std::setw(max_lenght) << state[i].beta << " | "
+          << std::setw(max_lenght) << state[i].gamma << " | " << '\n';
   }
-  ostrm << "+-----------+-----------+-----------+-----------+-----------+\n";
+  ostrm << bar << "\n";
 
   ostrm.close();
-};
+}
+}  // namespace print_on_file
 
 int main() {
   World_state initial_state;
@@ -50,8 +115,8 @@ int main() {
   std::ifstream istrm("data.txt");
   if (!istrm.is_open() && choice_1 == 'y') {
     std::cerr << "failed to open data.txt" << '\n';
-    choice_1 =
-        'n';  // il programma va avanti, ma è mandatorio mettere di dati a mano
+    choice_1 = 'n';  // il programma va avanti, ma è mandatorio mettere di
+                     // dati a mano
   }
   choice_1 = static_cast<int>(choice_1);
 
@@ -92,34 +157,40 @@ int main() {
   choice_2 = static_cast<int>(choice_2);
   switch (choice_2) {
     case 110:  // corrisponde al char n(no)
-      print(a);
+      print::print(a);
       break;
 
     case 97:  // corrisponde al char a (all)
-      print(a);
-      print_on_file(a);
+      print::print(a);
+      print_on_file::print_on_file(a);
       break;
 
     case 121:  // corrisponde al char y (yes)
-      print_on_file(a);
+      print_on_file::print_on_file(a);
       break;
 
     default:
-      print(a);
-      print_on_file(a);
+      print::print(a);
+      print_on_file::print_on_file(a);
       break;
   }
 
-  int start;
-  int new_duration_in_days;
-  double beta;
-  double gamma;
-  std::cin >> start >> new_duration_in_days >> beta >> gamma;
-  auto b = evolve(evolve(sir.get_state(), sir.get_duration()), start,
-                  new_duration_in_days, beta, gamma);
-  for (int i = 0; i < duration_in_days; ++i) {
-    b[i] = approx(b[i]);
-    assert(b[i].S + b[i].I + b[i].R - b[i].N == 0);
+  char choice_3;
+  std::cin >> choice_3;
+  choice_3 = static_cast<int>(choice_3);
+  if (choice_3 == 121) {
+    int start;
+    int new_duration_in_days;
+    double beta;
+    double gamma;
+    std::cin >> start >> new_duration_in_days >> beta >> gamma;
+    auto b = evolve(evolve(sir.get_state(), sir.get_duration()), start,
+                    new_duration_in_days, beta, gamma);
+    for (int i = 0; i < b.size(); ++i) {
+      b[i] = approx(b[i]);
+      assert(b[i].S + b[i].I + b[i].R - b[i].N == 0);
+    }
+    print::print(b);
+  } else {
   }
-  print(b);
 }
