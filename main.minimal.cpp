0a355fbaf90381951bf4b030c872fb869283c6c3
Matteo Bonazzi <matteo.bonazzi2@studio.unibo.it>
2021-07-09 22:40:08 +0200

    Updated to match with updates on main.cpp

--- main.minimal.cpp
+++ main.minimal.cpp
@@ -3,21 +3,56 @@
 #include <iostream>
 
 #include "pandemic.hpp"
-//#include"pandemic.hpp"
+
+int number_of_digits(int const number) {
+  int i = 1;
+  int N = number;
+  for (; N / 10 >= 1; ++i) {
+    N = N / 10;
+  }
+  return i;
+}
 
 void print(std::vector<World_state> const& state) {
-  std::cout << "+-----------+-----------+-----------+-----------+-----------+\n"
-               "|    Day    |     S     |     I     |     R     |     N     |\n"
-               "+-----------+-----------+-----------+-----------+-----------+\n"
-            << std::fixed;
-  for (int i = 0; i < static_cast<int>(state.size()); ++i) {
-    std::cout << std::setprecision(0) << "| " << std::setw(9) << i << " | "
-              << std::setw(9) << state[i].S << " | " << std::setw(9)
-              << state[i].I << " | " << std::setw(9) << state[i].R << " | "
-              << std::setw(9) << state[i].N << " |\n";
+  int max_lenght =
+      std::max(number_of_digits(state[0].N), number_of_digits(state.size()));
+  if (max_lenght % 2 == 0) {
+    ++max_lenght;
   }
-  std::cout << "+-----------+-----------+-----------+-----------+-----------+\n"
-            << state.size();
+  if (max_lenght < 5) {
+    max_lenght = 5;
+  }
+  std::string bar = "+";
+  std::string legend = "|";
+  std::vector<std::string> variables = {"Day", "S", "I", "R", "N", "B", "G"};
+  for (int b = 0; b < 7; ++b) {
+    for (int a = 1; a <= max_lenght + 2; ++a) {
+      bar.append("-");
+    }
+    for (int c = 0;
+         c <= static_cast<int>((max_lenght - variables[b].length()) / 2); ++c) {
+      legend.append(" ");
+    }
+    legend.append(variables[b]);
+    for (int c = 0;
+         c <= static_cast<int>((max_lenght - variables[b].length()) / 2); ++c) {
+      legend.append(" ");
+    }
+    bar.append("+");
+    legend.append("|");
+  }
+  std::cout << bar << '\n' << legend << '\n' << bar << '\n';
+  int size = state.size();
+  for (int i = 0; i < size; ++i) {
+    std::cout << "| " << std::setw(max_lenght) << i << " | "
+              << std::setw(max_lenght) << state[i].S << " | "
+              << std::setw(max_lenght) << state[i].I << " | "
+              << std::setw(max_lenght) << state[i].R << " | "
+              << std::setw(max_lenght) << state[i].S + state[i].I + state[i].R
+              << " | " << std::setw(max_lenght) << state[i].beta << " | "
+              << std::setw(max_lenght) << state[i].gamma << " | " << '\n';
+  }
+  std::cout << bar << "\n";
 }
 
 int main() {
